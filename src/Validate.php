746cba78bd6a3e26581617c7140c3cd5e67d16ec
Nestor Fabricio Parra <fabri0176@gmail.com>
2020-10-16 22:36:00 -0500

    Test password

--- src/Validate.php
+++ src/Validate.php
@@ -6,4 +6,13 @@ class Validate{
     public static function email($value){
         return (bool) filter_var($value, FILTER_VALIDATE_EMAIL);
     }
+
+    public static function url($url){
+        return (bool) filter_var($url, FILTER_VALIDATE_URL);
+    }
+
+    public static function password($password){
+        $valid = boolval(preg_match("/^[a-z0-9]{6,30}$/",$password));
+        return $valid;
+    }
 }
--- test/ValidacionTest.php
+++ test/ValidacionTest.php
@@ -4,11 +4,31 @@ use PHPUnit\Framework\TestCase;
 use App\Validate;
 class ValidateTest extends TestCase{
     // php vendor/phpunit/phpunit/phpunit
-    public function test_email(){
+    
+    public function testEmail(){
         $email = Validate::email('email@email.com');
         $this->assertTrue($email);
 
         $email = Validate::email('email@..@email.com');
         $this->assertFalse($email);
+    }
+
+    public function testUrl(){
+        $url = Validate::url("https://pagina.com");
+        $this->assertTrue($url);
+
+        $url = Validate::url("5534545");
+        $this->assertFalse($url);
+    }
+
+    public function testPassword(){
+        $password = Validate::password("334432424ffdsfds");
+        $this->assertTrue($password);
+
+        $password = Validate::password("2");
+        $this->assertFalse($password);
+
+        $password = Validate::password("niño");
+        $this->assertFalse($password);
     }
 }
